3ae3aeccda5abbb7223904997b315814814f25ff
AitorGH13 <148903609+AitorGH13@users.noreply.github.com>
2025-02-07 13:02:59 +0100

    Update TwitchConfig.php

--- TwitchConfig.php
+++ TwitchConfig.php
@@ -1,6 +1,6 @@
 <?php
 class TwitchConfig {
-    // Aquí defines tu Client ID y tu OAuth Token de Twitch
+    // Definimos nuestro Client ID y nuestro OAuth Token de Twitch
     const CLIENT_ID = 'iw4dxrhn2yqaethe9b6uwdbanf3xiw';
     const OAUTH_TOKEN = 'n2rnsruj57culzwz2iznqx6y5jbata';
 }
