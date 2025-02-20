5aeb3ffe9b441fde2a2305acc483f064bd26ba9c
Antenastyle <antenastyle@gmail.com>
2025-02-20 15:52:47 +0100

    Refactorización del código

--- index.php
+++ index.php
@@ -10,66 +10,39 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
 if (preg_match('/^\/analytics\/user$/', $uri) && ($method == 'GET')) {
+    $headers = getallheaders();
+    $token = $headers['Authorizationt'] ?? null;
     $id = $_GET['id'] ?? null;
-
-    if (!$id) {
-        http_response_code(400);
-        echo json_encode(["error" => "Invalid or missing 'id' parameter."]);
-        exit;
-    }
-
-    echo json_encode(StreamerController::getStreamerById($id));
+    echo json_encode(StreamerController::getStreamerById($id, $token));
     exit;
 }
 
 if (preg_match('/^\/analytics\/streams$/', $uri) && ($method == 'GET')) {
-    echo json_encode(StreamController::getLiveStreams());
+    $headers = getallheaders();
+    $token = $headers['Authorizationt'] ?? null;
+    echo json_encode(StreamController::getLiveStreams($token));
     exit;
 }
 
 if (preg_match('/^\/analytics\/streams\/enriched$/', $uri) && ($method == 'GET')) {
+    $headers = getallheaders();
+    $token = $headers['Authorizationt'] ?? null;
     $limit = $_GET['limit'] ?? 3;
-    echo json_encode(StreamController::getTopEnrichedStreams($limit));
+    echo json_encode(StreamController::getTopEnrichedStreams($limit, $token));
     exit;
 }
 
 if (preg_match('/^\/register$/', $uri) && ($method == 'POST')) {
     $data = json_decode(file_get_contents('php://input'), true);
-    $email = $data['email'];
-    
-    if (!isset($email)) {
-        http_response_code(400);
-        echo json_encode(["error" => "The email is mandatory."]);
-        exit;
-    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        http_response_code(400);
-        echo json_encode(["error" => "The email must be a valid email address."]);
-        exit;
-    }
-
+    $email = $data['email'] ?? null;
     echo json_encode(AuthController::registerEmail($email));
     exit;
 }
 
 if (preg_match('/^\/token$/', $uri) && ($method == 'POST')) {
     $data = json_decode(file_get_contents('php://input'), true);
-    $email = $data['email'];
-    $apiKey = $data['api_key'];
-    
-    if (!isset($email)) {
-        http_response_code(400);
-        echo json_encode(["error" => "The email is mandatory."]);
-        exit;
-    } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        http_response_code(400);
-        echo json_encode(["error" => "The email must be a valid email address."]);
-        exit;
-    } else if (!isset($apiKey)) {
-        http_response_code(400);
-        echo json_encode(["error" => "The api_key is mandatory."]);
-        exit;
-    }
-
+    $email = $data['email'] ?? null;
+    $apiKey = $data['api_key'] ?? null;
     echo json_encode(AuthController::createAccessToken($email, $apiKey));
     exit;
 }
