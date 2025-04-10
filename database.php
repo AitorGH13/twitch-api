ebd562ba9a726e679eeeab9d337d14eb3dfa6ce4
Antenastyle <39882188+Antenastyle@users.noreply.github.com>
2025-04-10 02:02:15 +0200

    9 modificaciones para cumplir con los requisitos de la entrega 2 (#10)
    * Modificados endpoints para admitir únicamente Bearer Token
    
    - Se ha eliminado el header Authorizationt.
    - Se ha eliminado el uso de Basic-Auth de CDMon
    
    * Modificada contraseña de acceso a la BD y el charset
    
    * Update README.md

--- database.php
+++ database.php
@@ -7,10 +7,10 @@ class Database {
             $host = "localhost";
             $dbname = "twitchanalytics";
             $username = "root";
-            $password = "";
+            $password = "dxl2025*";
             
             try {
-                self::$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+                self::$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
                 self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
                 die(json_encode(["error" => "Database connection failed: " . $e->getMessage()]));
