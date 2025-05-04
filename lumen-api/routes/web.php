e0041eb3dce88105b6667dd7bd0486c3efe443c8
Lauder01 <raulitoizg@gmail.com>
2025-05-04 22:34:10 +0200

    [#35] - Solucionado problema de rutas de Lumen

---
+++ lumen-api/routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->post('/register', function (\Illuminate\Http\Request $request) {
+    return response()->json(['message' => 'User registered successfully', 'email' => $request->input('email')]);
+});
+
+$router->post('/token', function (\Illuminate\Http\Request $request) {
+    return response()->json(['token' => 'generated_token', 'email' => $request->input('email')]);
+});
+
+$router->get('/analytics/user', function (\Illuminate\Http\Request $request) {
+    $userId = $request->input('id');
+    return response()->json(['id' => $userId, 'name' => 'Sample User']);
+});
+
+$router->get('/analytics/streams', function () {
+    return response()->json(['streams' => ['stream1', 'stream2']]);
+});
+
+$router->get('/analytics/topsofthetops', function () {
+    return response()->json(['top_games' => ['game1', 'game2', 'game3']]);
+});
+
+$router->get('/analytics/streams/enriched', function (\Illuminate\Http\Request $request) {
+    $limit = $request->input('limit', 3);
+    return response()->json(['enriched_streams' => array_fill(0, $limit, 'enriched_stream')]);
+});
