8beb2dd1ec0d06e328468ad7226bcd89f4b20a90
Raúl Itoiz <63600546+Lauder01@users.noreply.github.com>
2025-05-04 22:46:48 +0200

    [#35] - Solucionado problema de rutas de Lumen (#36)
    * [#35] - Solucionado problema de rutas de Lumen
    
    * [#35] - Solucionado problema de rutas de Lumen
    
    * [#35] - Solucionado problema de rutas de Lumen

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,14 +1,15 @@
 <?php
+
 use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
+
+$finder = Finder::create()
+    ->in(__DIR__)
+    ->exclude('vendor')
+    ->exclude('lumen-api');
 
 return (new Config())
     ->setRules([
         '@PSR12' => true,
-        // aquí puedes añadir reglas extras si quieres
     ])
-    ->setFinder(
-        Finder::create()
-            // apunta a tu carpeta de código
-            ->in(__DIR__ . '/src')
-    );
+    ->setFinder($finder);
