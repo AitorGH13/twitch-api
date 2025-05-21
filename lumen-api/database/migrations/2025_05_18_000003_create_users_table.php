54c5a588f5c9aee1f62bc4b655729d61f81023cc
AitorGH13 <148903609+AitorGH13@users.noreply.github.com>
2025-05-21 22:16:36 +0200

    [#53] - Clean create_users_table.php

--- lumen-api/database/migrations/2025_05_18_000003_create_users_table.php
+++ lumen-api/database/migrations/2025_05_18_000003_create_users_table.php
@@ -1,7 +1,5 @@
 <?php
 
-// database/migrations/2025_05_18_000003_create_users_table.php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
