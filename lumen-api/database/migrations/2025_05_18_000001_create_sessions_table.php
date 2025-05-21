c9607f66d33773819c3f6cced8a8201236805b1b
AitorGH13 <148903609+AitorGH13@users.noreply.github.com>
2025-05-21 22:16:09 +0200

    [#53] - Clean create_sessions_table.php

--- lumen-api/database/migrations/2025_05_18_000001_create_sessions_table.php
+++ lumen-api/database/migrations/2025_05_18_000001_create_sessions_table.php
@@ -1,7 +1,5 @@
 <?php
 
-// database/migrations/2025_05_18_000001_create_sessions_table.php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
