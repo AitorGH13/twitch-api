e06f3e78e844759974e40af88f98804f008f6d94
AitorGH13 <148903609+AitorGH13@users.noreply.github.com>
2025-05-23 11:20:01 +0200

    [#60] - Creado TwitchManager y cambios en Service y Database

--- app/Services/TokenService.php
+++ app/Services/TokenService.php
@@ -25,13 +25,11 @@ class TokenService
             throw new InvalidApiKeyException();
         }
 
-        // Intentamos obtener token existente (y su expiración)
         $response = $this->manager->getToken($userId);
         $data     = json_decode($response->getContent(), true);
         $token    = $data['token'];
         $expires  = $data['expires_at'];
 
-        // Si no había token o ha expirado, generamos uno nuevo
         if (! $token || strtotime($expires) < time()) {
             $newResp = $this->manager->generateToken();
             $newData = json_decode($newResp->getContent(), true);
