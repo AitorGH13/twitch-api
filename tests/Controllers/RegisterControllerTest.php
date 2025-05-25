a04c871ad6915f2afa0689b7400408cc5980a8d5
Antenastyle <antenastyle@gmail.com>
2025-05-25 14:22:59 +0200

    [#78] - Test para el registro con el mismo email

--- tests/Controllers/RegisterControllerTest.php
+++ tests/Controllers/RegisterControllerTest.php
@@ -57,4 +57,21 @@ class RegisterControllerTest extends TestCase
         $body = json_decode($this->response->getContent(), true);
         $this->assertEquals(32, strlen($body['api_key']));
     }
+
+    /** @test */
+    public function registeringSameEmailReturnsDifferentApiKey()
+    {
+        $email = 'user@example.com';
+        $this->post('/register', ['email' => $email]);
+        $this->seeStatusCode(200);
+        $firstBody = json_decode($this->response->getContent(), true);
+        $this->assertArrayHasKey('api_key', $firstBody);
+        $firstApiKey = $firstBody['api_key'];
+        $this->post('/register', ['email' => $email]);
+        $this->seeStatusCode(200);
+        $secondBody = json_decode($this->response->getContent(), true);
+        $this->assertArrayHasKey('api_key', $secondBody);
+        $secondApiKey = $secondBody['api_key'];
+        $this->assertNotEquals($firstApiKey, $secondApiKey);
+    }
 }
